9a4cf7ce97828f5e98faebd0da9ba0f987f73751
timon-yego <kipyegotimothy99@gmail.com>
2025-02-06 14:11:39 +0300

    Modified alx_travel_app/urls.py

--- alx_travel_app/alx_travel_app/urls.py
+++ alx_travel_app/alx_travel_app/urls.py
@@ -14,8 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin, include
-from django.urls import path,  re_path
+from django.contrib import admin
+from django.urls import path,  re_path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
